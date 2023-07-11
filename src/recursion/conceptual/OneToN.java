4e5f6937d93f7913616e20e76c3cee5ea2ee5d7f
Sahil Suman <sahilsuman933@gmail.com>
2023-07-11 12:39:31 +0530

    check
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

---
+++ src/recursion/conceptual/OneToN.java
@@ -0,0 +1,13 @@
+package recursion.conceptual;
+
+import java.util.Arrays;
+
+public class OneToN {
+
+    
+
+    public static void main(String[] args) {
+        int[] arr = printOneToN(5);
+        System.out.println(Arrays.toString(arr));
+    }
+}
